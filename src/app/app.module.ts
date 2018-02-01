6a36c33366e2a7d450902e2f8e607fb8947fb6ad
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-01 19:22:28 +0900

    add import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
