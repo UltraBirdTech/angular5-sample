4d177fada57f1dc022652060443a90b6e513e82c
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-04 07:30:30 +0900

    fix typo

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInt } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styuleUrls: []
+  templateUrl: './dashboard.component.html'
+//  styuleUrls: []
 })
 
-export class DashboardComponent implements OnInt {
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInt() {
+  ngOnInit() {
     this.getHeroes();
   }
 
