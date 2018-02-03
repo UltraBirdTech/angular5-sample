3d1614086f9bd08bcefee96e840f347dbd115d4e
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-03 16:38:56 +0900

    fix typo

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,7 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
-  createDB(){
+  createDb() {
     const heroes = [
       { id: 11, name: 'Mr. Nice' },
       { id: 12, name: 'Narco' },
