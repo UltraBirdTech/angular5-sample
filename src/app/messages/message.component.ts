1d1fa50f6dfa05f823dbeddaab45d45e6b18446b
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-11 20:03:53 +0900

    fix typo

--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -4,7 +4,7 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styuleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.css']
 })
 export class MessagesComponnet implements OnInit {
   constructor(public messageService: MessageService) {}
