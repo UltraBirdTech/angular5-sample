6264458ffa90ba18c9452cefb0ba51546b9d4c4f
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-11 14:28:41 +0900

    fix typo

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,7 +9,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
-const HttpOptions = {
+const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
@@ -61,7 +61,7 @@ export class HeroService {
   }
 
   addHero( hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, HttpOptions).pipe(
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
