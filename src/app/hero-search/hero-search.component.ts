b0e22d672790bcaf577fa13872d7ba8c6bb615c9
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-12 08:25:58 +0900

    fix typo(s)

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -5,30 +5,32 @@ import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs/observable/of';
 
 import { 
-  debouceTime, distinctUntilChanged, switchMap
+  debounceTime, distinctUntilChanged, switchMap
  } from 'rxjs/operators';
 
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
 
-@Conmponent({
+@Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  tyuleUrls: []
-)}
+  styleUrls: []
+})
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {}
 
   search(term: string): void{
     this.searchTerms.next(term);
   }
 
   ngOnInit(): void {
-    this.heroes$ = this.serchTerms.pipe(
+    this.heroes$ = this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((term: string) => this.heroSearchHeroes(term)),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
     );
   }
 }
