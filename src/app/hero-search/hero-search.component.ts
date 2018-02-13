b0b9d6ff4bd49baa7bc327f21d383c941c33a1e2
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-02-14 06:28:20 +0900

    add css setting

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -14,7 +14,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: []
+  styleUrls: ['./hero-search.component.css' ]
 })
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
